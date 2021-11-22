ead5ef52cc4479782ec3feb5d6527c52ccdf8e48
Conor <driscollconor3@gmail.com>
2021-11-22 17:47:39 -0500

    Set 7 Finished

--- src/com/foundry/AlgorithmSet7/FindNumberofDigits.java
+++ src/com/foundry/AlgorithmSet7/FindNumberofDigits.java
@@ -2,4 +2,8 @@ package com.foundry.AlgorithmSet7;
 
 public class FindNumberofDigits {
     //https://edabit.com/challenge/z9ofdqhTYi9tdCj5T
+    public static int numOfDigits(int num) {
+        return String.valueOf(num).length();
+    }
+
 }
